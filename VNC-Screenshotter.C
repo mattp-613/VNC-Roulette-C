2468006c9c010a4e678ec768dc93cb6d2548fd2e
mattp.613 <sperky7933@gmail.com>
2023-06-01 08:42:16 -0400

    added semaphore for file writing need to turn these into functions later on for claity

--- VNC-Screenshotter.C
+++ VNC-Screenshotter.C
@@ -18,14 +18,15 @@
 
 //OPTIONS
 #define LABEL_IPS 1 //Should there be ip list of vulrenable and/or non vulnerable IP's created?
-#define SAVE_IPS 1 //should we use an ip_left list to remove IP's already classified as vulernable/non-vulnerable?
+
 
 sem_t *thread_sem; // semaphore for limiting forks
+sem_t *write_sem; // semaphore for limiting file writing (a file lock)
 
 int main() {
 	char ip[17];
 	FILE *fp;
-
+	FILE *fs;
 
     thread_sem = sem_open("/thread", O_CREAT | O_EXCL, 0644, MAX_THREADS); //Named semaphore required for parent-child intercommunication
 
@@ -39,27 +40,38 @@ int main() {
         return 1;
     }
 
+	//Writing to file semaphore
+	//This is to ensure only one thread writes to a file at a time!
+	
+	write_sem = sem_open("/write", O_CREAT | O_EXCL, 0645, 1); //Named semaphore required for parent-child intercommunication
+
+	if (write_sem == SEM_FAILED) {
+        perror("Error creating semaphore");
+        return 1;
+    }
+
+	if (sem_unlink("/write") == -1) { //This is required to prevent the semaphore from lasting forever
+        perror("Error unlinking semaphore");
+        return 1;
+    }
+
 	fp = fopen(IP_LIST, "r");
 	if (fp == NULL) {
 		perror("Error opening file");
 		return 1;
 	}
 
-	if(SAVE_IPS == 1){
-		FILE *fs;
-		fs = fopen(IP_LEFT, "r");
-		if (fs == NULL) {
-			//TODO: Copy IP_LIST To a new file IP_LEFT
+	fs = fopen(IP_LEFT, "r");
+	if (fs == NULL) {
+		//TODO: Copy IP_LIST To a new file IP_LEFT
 		}
-	}
 
 	while(fgets(ip, 17, fp)) {
 		printf("Scanning %s",ip);
 		ip[strcspn(ip, "\n")] = 0; // remove newline character
 
-		char command[96];
-		sprintf(command, "timeout 10 vncsnapshot -quiet -rect 0x0-800-600 %s:0 snapshot%s.jpg", ip, ip);
-
+		char command[69];
+		sprintf(command, "timeout 10 vncsnapshot -quiet -rect 0x0-800-600 %s:0 snapshot_%s.jpg", ip, ip);
 		if (sem_wait(thread_sem) == -1) {
             perror("Error waiting on semaphore");
             return 1;
@@ -67,8 +79,17 @@ int main() {
 
 		if(fork() == 0) {
 			
+			if (sem_wait(write_sem) == -1) {
+            perror("Error waiting on semaphore");
+            return 1;
+        	}
+
 			//TODO: remove ip from ipsleft.txt
-			//use a file lock and remove index 0! this SHOULD Work!!!!!
+			
+			if (sem_post(write_sem) == -1) {
+                perror("Error releasing semaphore");
+                return 1;
+            }
 
 			int timeout = system(command);
 
@@ -87,7 +108,8 @@ int main() {
                 return 1;
             }
 
-			sem_close(thread_sem);
+			sem_close(thread_sem); //TODO: Move fclose, semclose, etc to a CTRL+C exception
+			sem_close(write_sem);
 			exit(0);
 
 		}
@@ -95,5 +117,6 @@ int main() {
 	}
 
 	fclose(fp);
+	fclose(fs);
 	return 0;
 }
